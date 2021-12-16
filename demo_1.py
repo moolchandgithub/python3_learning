4099f604841cef89c3769f63e1528429650f0f83
Moolchand <mool.attrish@gmail.com>
2021-12-16 17:00:47 +0200

    Demo_1 file updated

--- demo_1.py
+++ demo_1.py
@@ -1,35 +1,35 @@
-# my_sqrs = [i ** 2 for i in range(3)]
-# print(my_sqrs)
+my_sqrs = [i ** 2 for i in range(3)]
+print(my_sqrs)
 
-# cars = ['ford', 'gmc', 'bmw', 'fiat', 'kia']
-# print([car.upper()for car in cars])
+cars = ['ford', 'gmc', 'bmw', 'fiat', 'kia']
+print([car.upper()for car in cars])
 
-# print([car[:2]for car in cars])
+print([car[:2]for car in cars])
 
-# print([car for car in cars if car.startswith('f')])
+print([car for car in cars if car.startswith('f')])
 
-# r = list(range(1,10,2))
-# print(r)
+r = list(range(1,10,2))
+print(r)
 
-#help("keywords")
+help("keywords")
 
-# num = 123.4456
-# int_num = 150
+num = 123.4456
+int_num = 150
 
-# print(f"The value is {num} and number is {int_num}")
-# print("The value of flot %.2f and int is %d" %(num, int_num))
+print(f"The value is {num} and number is {int_num}")
+print("The value of flot %.2f and int is %d" %(num, int_num))
 
-# x = "hello world!"
-# y = "HELLO WORLD!"
+x = "hello world!"
+y = "HELLO WORLD!"
 
-# print(x.replace("h", "S"))
-# print(y)
+print(x.replace("h", "S"))
+print(y)
 
-# print("#"*30)
+print("#"*30)
 
-# print(dir(__builtins__))
-# _x = 5
-# print(_x)
+print(dir(__builtins__))
+_x = 5
+print(_x)
 x = 1  
 while True:  
    if x % 5 == 0:  
@@ -48,6 +48,6 @@ while True:
 # except:  
 #   print("error comes in the line")
 
-# print('2' == 2)
+print('2' == 2)
 
 # x = [10, 20 ,30]
